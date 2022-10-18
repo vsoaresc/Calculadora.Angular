47ba21b2c13b90c24ab569849adf1dd5560121d3
Vitor Soares <58884615+vsoaresc@users.noreply.github.com>
2022-10-17 22:46:56 -0300

    instalação bootstrap 3 e configurações de index

--- calculadora/src/app/calculadora/calculadora.module.ts
+++ calculadora/src/app/calculadora/calculadora.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CalculadoraComponent } from './components/';
-
-
+import { CalculadoraService } from './services';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,9 @@ import { CalculadoraComponent } from './components/';
   ],
   exports:[
     CalculadoraComponent
+  ],
+  providers:[
+    CalculadoraService
   ]
 })
 export class CalculadoraModule { }
