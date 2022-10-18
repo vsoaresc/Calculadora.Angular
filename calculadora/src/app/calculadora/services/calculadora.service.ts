9023418a940a5d74939c2355084c989e9d350eec
Vitor Soares <58884615+vsoaresc@users.noreply.github.com>
2022-10-17 23:00:44 -0300

    criação service e referência bootstrap angular.json

--- calculadora/src/app/calculadora/services/calculadora.service.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { defaultThrottleConfig } from 'rxjs/internal/operators/throttle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
 
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  calcular(num1: number, num2: number, operacao: string): number{
+    let resultado: number;
+
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+        break;
+        case CalculadoraService.SOMA:
+        resultado = num1 - num2;
+        break;
+        case CalculadoraService.SOMA:
+        resultado = num1 / num2;
+        break;
+        case CalculadoraService.SOMA:
+        resultado = num1 * num2;
+        break;
+        default:
+          resultado = 0;
+    }
+
+    return resultado;
+  }
 }
